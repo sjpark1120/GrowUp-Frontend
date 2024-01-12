a7d7048905d3d2b7a138e64a9750b613de91d24e
ji1won <wony3512@naver.com>
2024-01-13 08:13:19 +0900

    writepage
    writepage 추가했습니다.

--- src/pages/GrowRoomPage/GrowRoomPostPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GrowRoomPostPage() {
-  return (
-    <div>GrowRoom 모집글</div>
-  )
-}
-
-export default GrowRoomPostPage
--- src/pages/MainPage/MainPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MainPage() {
-  return (
-    <div>Main입니다</div>
-  )
-}
-
-export default MainPage
