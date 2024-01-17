d0ecc16ba733394ced327756cce0c4300fb01f4a
aong13 <oaoa13@pukyong.ac.kr>
2024-01-17 20:01:57 +0900

    feat: add Banner at mainPage

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import styled from 'styled-components';
+
+import banner from '../../icon/banner2.png'
+
+const BannerImg =styled.img`
+background-image: url(${banner});
+background-size: cover;
+width: 100%;
+height: 500px;
+  `;
+
+const MainWrapper = styled.div`
+  width: 1220px;
+  align-items: center;
+  justify-content: center;
+  margin: 100px auto;
+`;
+
 
 function MainPage() {
   return (
-    <div>Main입니다</div>
+    <div>
+    <BannerImg />
+    <MainWrapper>
+
+    </MainWrapper>
+
+    </div>
   )
 }
 
