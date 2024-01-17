af78b1ffa505109ba2b07fa3c1f5d2fd77b859ca
aong13 <oaoa13@pukyong.ac.kr>
2024-01-17 21:34:41 +0900

    css: dropdown css

--- src/components/GrowRoom/DropDown.jsx
+++ src/components/GrowRoom/DropDown.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import down from '../../icon/arrow_dropdown.png';
+import down_white from '../../icon/arrow_down_white.png';
 import DropDownOption from './DropDownOption';
 
 const Container = styled.div`
   position: relative;
-`
+`;
+
 const DropDownContainer = styled.div`
   position: absolute;
-  top: 100%; 
+  top: 100%;
   display: flex;
   padding-top: 10px;
   gap: 5px;
@@ -22,50 +24,61 @@ const DropDownHeader = styled.div`
   justify-content: space-between;
   align-items: center;
   border-radius: 30px;
-  border: 1px solid #B0B0B0;
-  background: #FFF;
+  border: 1px solid ${({ isOpen }) => (isOpen ? '#00D749' : '#B0B0B0')};
+  background: ${({ isOpen }) => (isOpen ? '#00D749' : '#FFF')};
+  color: ${({ isOpen }) => (isOpen ? '#FFF' : '#3E3E3E')};
   font-size: 14px;
   font-weight: 500;
   line-height: 140%;
+  cursor: pointer;
 `;
 
+const ArrowIcon = styled.img`
+  transition: transform 0.3s;
+  transform: ${({ isOpen }) => (isOpen ? 'rotate(180deg)' : 'rotate(0deg)')};
+`;
 const Dropdown = ({ title, optionsMap, options }) => {
   const [mainDropdownOpen, setMainDropdownOpen] = useState(false);
-  const [detailDropdownOpen, setDetailDropdownOpen] = useState(false);
   const [selectedMain, setSelectedMain] = useState('');
 
   const toggleMainDropdown = () => {
     setMainDropdownOpen(!mainDropdownOpen);
-    setDetailDropdownOpen(optionsMap ? true : false);
+    setSelectedMain('');
   };
 
   const mainSelected = (option) => {
-    setSelectedMain(option);
     if (!optionsMap) {
-      setMainDropdownOpen(false);
+      setTimeout(() => {
+        setMainDropdownOpen(false);
+        setSelectedMain('');
+      }, 500);
+    }
+    else{
+      setSelectedMain(option);
     }
   };
 
   const detailSelected = (option) => {
-    setSelectedMain('');
-    setDetailDropdownOpen(false);
-    setMainDropdownOpen(false);
+    setTimeout(() => {
+      setMainDropdownOpen(false);
+      setSelectedMain('');
+    }, 500);
   };
 
   return (
     <Container>
-      <DropDownHeader onClick={toggleMainDropdown}>
+      <DropDownHeader isOpen={mainDropdownOpen} onClick={toggleMainDropdown}>
         {title}
-        <img src={down} alt="drop down arrow" />
+        <ArrowIcon isOpen={mainDropdownOpen} src={mainDropdownOpen ? down_white : down} alt="drop down arrow" />
       </DropDownHeader>
-      <DropDownContainer>
+      <DropDownContainer isOpen={mainDropdownOpen}>
         {mainDropdownOpen && optionsMap && (
           <DropDownOption options={Object.keys(optionsMap)} onClick={mainSelected} />
         )}
         {mainDropdownOpen && options && !optionsMap && (
           <DropDownOption options={options} onClick={mainSelected} />
         )}
-        {selectedMain && detailDropdownOpen && (
+        {selectedMain && (
           <DropDownOption options={optionsMap ? optionsMap[selectedMain] : options} onClick={detailSelected} />
         )}
       </DropDownContainer>
