2bd96e399844824de82d0c2be006e18d3692b0e4
sujinpark <asgol1120@naver.com>
2024-01-19 19:15:32 +0900

    feat: post 컴포넌트 누르면 화면공유페이지로 이동

--- src/pages/LiveUpPage/LiveUpPage.jsx
+++ src/pages/LiveUpPage/LiveUpPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import banner from '../../icon/banner1.png'
-import PostBoxBlack from '../../components/LiveUpPage/PostBoxBlack';
-import UserRanking from '../../components/LiveUpPage/UserRanking';
 import LiveUpNavigation from '../../components/LiveUpPage/LiveUpNavigation';
 import RankingFilterBtn from '../../components/LiveUpPage/RankingFilterBtn';
 import RankingBox from '../../components/LiveUpPage/RankingBox';
@@ -21,15 +19,6 @@ const BannerImg =styled.img`
   height: 500px;
   `;
 
-const PostBoxContainer = styled.div`
-  width: 100%;
-  max-width: 1220px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 40px 16px;
-  margin-top: 50px;
-`
-
 const RankingContainer = styled.div`
   width: 1220px;
   height: 180px;
@@ -39,17 +28,7 @@ const MyLiveUpContainer = styled.div`
   width: 1220px;
   margin-top: 150px;
 `
-const RankingBtn = styled.button`
-  border: 0;
-  background-color: transparent;
-  width: 57px;
-  height: 42px;
-  border: 1px solid #ffffff;
-  border-radius: 30px;
-  color: #ffffff;
-  margin-right: 10px;
-  cursor: pointer;
-`
+
 const RankingTitle = styled.h1`
   color:#FFF;
   font-size: 25px;
@@ -78,10 +57,10 @@ function LiveUpPage() {
         <RankingBox />
       </RankingContainer>
       <MyLiveUpContainer>
-      <MyLiveUpTitle>MY LIVE UP</MyLiveUpTitle>
-      <LiveUpNavigation/>
-      <LiveUpPagination/>
-    </MyLiveUpContainer>
+        <MyLiveUpTitle>MY LIVE UP</MyLiveUpTitle>
+        <LiveUpNavigation />
+        <LiveUpPagination />
+      </MyLiveUpContainer>
     </MainWrapper>
   )
 }
